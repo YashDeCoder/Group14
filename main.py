e902cf4c67cb8df7dd1235a45ba3234ebc40038c
Yash <yashisrani35@gmail.com>
2024-06-08 18:00:33 +0200

    Try to remove noise from start and end

--- main.py
+++ main.py
@@ -34,6 +34,38 @@ def read_and_combine_datasets(directory):
     
     return concatenated_dataframes
 
+def filter_first_and_last_second(df):
+    sorted_df = df.sort_values(by='Time (s)')
+    start_time = sorted_df.iloc[0]
+    end_time = sorted_df.iloc[-1]
+    
+    # Filter rows for the first and last second
+    # THIS DOESN'T WORK WHY :(
+    first_second = sorted_df[sorted_df['Time (s)'] <= start_time + 1.0 ]
+    last_second = sorted_df[sorted_df['Time (s)'] >= end_time - 1.0]
+    filtered_df = pd.concat([first_second, last_second])
+    return filtered_df
+
+def plot_categories(dfs_by_xx):
+    for xx, df in dfs_by_xx.items():
+        filtered_df = filter_first_and_last_second(df)
+        
+        plt.figure(figsize=(14, 8))
+        plt.title(f'Categories Plot for First and Last Second - xx={xx}')
+        time_column = 'Time (s)'
+        
+        # List of columns to plot
+        categories = [col for col in df.columns if col != time_column]
+
+        for category in categories:
+            plt.plot(df[time_column], df[category], label=category)
+        
+        plt.xlabel('Time (s)')
+        plt.ylabel('Value')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
 # Example usage
 directory = 'data'
 dfs_by_xx = read_and_combine_datasets(directory)
@@ -43,4 +75,8 @@ df_0 = dfs_by_xx['0']
 df_10 = dfs_by_xx['10']
 df_30 = dfs_by_xx['30']
 df_60 = dfs_by_xx['60']
-print(df_0.head(10))
+
+# print(df_0['Time (s)'].dtype)
+
+# Example usage
+plot_categories(dfs_by_xx)
